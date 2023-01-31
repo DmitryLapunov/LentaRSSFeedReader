9ec24989696aa17a211f75f2f7f1c56be7e249b0
Dmitry Lapunov <diamondtheonly@gmail.com>
2023-01-31 21:26:32 +0300

    create blank project

---
+++ LentaRSSFeedReader/LentaRSSFeedReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LentaRSSFeedReader
+//
+//  Created by Дмитрий Лапунов on 31.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
